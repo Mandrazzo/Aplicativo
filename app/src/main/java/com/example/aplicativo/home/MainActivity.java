207f137e87421e892ef20ae9afdd9ace938d7cb9
brenomohr <breno.mohr1@gmail.com>
2023-10-30 21:41:42 -0300

    Inclusão de permissão de localização - Ainda não 100% funcional.

--- app/src/main/java/com/example/aplicativo/home/MainActivity.java
+++ app/src/main/java/com/example/aplicativo/home/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.aplicativo.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.compose.foundation.interaction.HoverInteraction;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -41,6 +49,36 @@ public class MainActivity extends AppCompatActivity {
             return true;
 
         });
-        // Fim
+
+         /* TENTATIVA DE SOLICITAR PERMISSÃO
+        class LocationActivity extends AppCompatActivity {
+            private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
+                // Verifica se a permissão de localização foi concedida
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    // A permissão já foi concedida
+                    // Você pode acessar a localização aqui
+                } else {
+                    // Solicita permissão de localização
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+                }
+            }
+
+
+           @Override
+            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+                if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        // A permissão de localização foi concedida pelo usuário
+                        // Agora você pode acessar a localização
+                    } else {
+                        // A permissão de localização foi negada pelo usuário
+                        finish(); */
     }
 }
+        // Fim
