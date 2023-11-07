3bcb76898c9d7a173f41ef942160b88290418a5a
brenomohr <breno.mohr1@gmail.com>
2023-11-07 01:53:17 -0300

    Inclusão de login continuo, verificações de login e adicionado db firebase com dados.

--- app/src/main/java/com/example/aplicativo/Login.java
+++ app/src/main/java/com/example/aplicativo/Login.java
@@ -4,66 +4,136 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.aplicativo.home.MainActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Login extends AppCompatActivity {
+    public class Login extends AppCompatActivity {
+        String[] mensagens = {"Preencha todos os campos", "Login efetuado com sucesso!"};
+        private EditText emailedt, senhaedt;
+        private ProgressBar progressBar;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
 
-        Button meuBotao = findViewById(R.id.buttonLogin);
-        Button souCliente = findViewById(R.id.cliente);
-        TextView cad = findViewById(R.id.cadastro);
-        TextView rec = findViewById(R.id.esqueciSenha);
+            IniciarComponentes();
 
-        // Click do botão para Logar
-        meuBotao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
+            Button meuBotao = findViewById(R.id.buttonLogin);
+            Button souCliente = findViewById(R.id.cliente);
+            TextView cad = findViewById(R.id.cadastro);
+            TextView rec = findViewById(R.id.esqueciSenha);
+
+            // Click do botão para Logar
+            meuBotao.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String email = emailedt.getText().toString();
+                    String senha = senhaedt.getText().toString();
+                    if (email.isEmpty() || senha.isEmpty()) {
+                        Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+                        snackbar.setBackgroundTint(Color.BLACK);
+                        snackbar.setTextColor(Color.WHITE);
+                        snackbar.show();
+                    } else {
+                        AutenticarUsuario(v);
+                    }
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+            // Click do botão para registro
+            cad.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Login.this, Registro.class);
+                    startActivity(intent);
+                }
+            });
+            // Click do botão para recuperação de senha
+            rec.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Login.this, Recuperacao.class);
+                    startActivity(intent);
+                }
+            });
+
+            souCliente.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Login.this, Rastreio.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        private void AutenticarUsuario(View view) {
+            String email = emailedt.getText().toString();
+            String senha = senhaedt.getText().toString();
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        progressBar.setVisibility(View.VISIBLE);
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                               TelaPrincipal();
+                            }
+                        }, 3000);
+                    } else {
+                        String erro;
+                        try {
+                            throw task.getException();
+                        } catch (Exception e) {
+                            erro = "Erro ao logar usuário";
+                        }
+                        Snackbar snackbar = Snackbar.make(view, erro, Snackbar.LENGTH_SHORT);
+                        snackbar.setBackgroundTint(Color.BLACK);
+                        snackbar.setTextColor(Color.WHITE);
+                        snackbar.show();
+                    }
+                }
+            });
+        }
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+
+            if(usuarioAtual !=null){
+                TelaPrincipal();
             }
-        });
-        // Click do botão para registro
-        cad.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Login.this, Registro.class);
-                startActivity(intent);
-            }
-        });
-        // Click do botão para recuperação de senha
-        rec.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Login.this, Recuperacao.class);
-                startActivity(intent);
-            }
-        });
+        }
 
-        souCliente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Login.this, Rastreio.class);
-                startActivity(intent);
-            }
-        });
+        private void TelaPrincipal(){
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
+        private void IniciarComponentes() {
+            emailedt = findViewById(R.id.emailedt);
+            senhaedt = findViewById(R.id.senhaedt);
+            progressBar = findViewById(R.id.progressBar); // Adicionei isso para inicializar a progressBar
+        }
     }
-}
-
 
--- app/src/main/java/com/example/aplicativo/Registro.java
+++ app/src/main/java/com/example/aplicativo/Registro.java
@@ -1,102 +1,149 @@
 package com.example.aplicativo;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.content.Intent;
-
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class Registro extends AppCompatActivity {
     private EditText edit_nome, edit_email, edit_senha, edit_renavam, edit_placa, edit_cpf, edit_data, edit_telefone;
     private Button btnCadastrar;
+
+
     String[] mensagens = {"Preencha todos os campos", "Cadastro realizado com sucesso!"};
+    String usuarioID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        IniciarComponentes();
+
         Button voltar = findViewById(R.id.voltar);
-        // Click do botão para voltar para tela de login
         voltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Registro.this, Login.class);
-                startActivity(intent);
-                getSupportActionBar().hide();
-                IniciarComponentes();
-
-                btnCadastrar.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        String nome = edit_nome.getText().toString();
-                        String email = edit_email.getText().toString();
-                        String senha = edit_senha.getText().toString();
-                        String placa = edit_placa.getText().toString();
-                        String renavam = edit_renavam.getText().toString();
-                        String data = edit_data.getText().toString();
-                        String cpf = edit_cpf.getText().toString();
-                        String telefone = edit_telefone.getText().toString();
-
-
-                        if (nome.isEmpty() || email.isEmpty() || senha.isEmpty() || renavam.isEmpty() || placa.isEmpty() || data.isEmpty() || cpf.isEmpty() || telefone.isEmpty()) {
-                            Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
-                            snackbar.setBackgroundTint(Color.WHITE);
-                            snackbar.setTextColor(Color.BLACK);
-                            snackbar.show();
-                        } else {
-                            CadastrarUsuario(v);
-                        }
-                    }
-
-
-                    private void CadastrarUsuario(View v) {
-                        String email = edit_email.getText().toString();
-                        String senha = edit_senha.getText().toString();
-
-                        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                                if (task.isSuccessful()) {
-                                    Snackbar snackbar = Snackbar.make(v, mensagens[1], Snackbar.LENGTH_SHORT);
-                                    snackbar.setBackgroundTint(Color.WHITE);
-                                    snackbar.setTextColor(Color.BLACK);
-                                    snackbar.show();
-                                }
-                            }
-                        });
-                    }
-                });
-            }
-
-            private void IniciarComponentes() {
-                IniciarComponentes();
-                edit_nome = findViewById(R.id.edit_nome);
-                edit_email = findViewById(R.id.edit_email);
-                edit_senha = findViewById(R.id.edit_senha);
-                edit_cpf = findViewById(R.id.edit_cpf);
-                edit_placa = findViewById(R.id.edit_placa);
-                edit_renavam = findViewById(R.id.edit_renavam);
-                edit_data = findViewById(R.id.edit_data);
-                edit_telefone = findViewById(R.id.edit_telefone);
-                btnCadastrar = findViewById(R.id.btnCadastrar);
-
+               // Intent intent = new Intent(Registro.this, Login.class);
+                //startActivity(intent);
             }
         });
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(Registro.this, Login.class);
+               // startActivity(intent);
+
+                String nome = edit_nome.getText().toString();
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
+                    Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_LONG);
+                    snackbar.setBackgroundTint(Color.BLACK);
+                    snackbar.setTextColor(Color.WHITE);
+                    snackbar.show();
+                } else {
+                    CadastrarUsuario(v);
+                }
+            }
+        });
+    }
+
+    private void SalvarDadosUsuario(){
+        String nome = edit_nome.getText().toString();
+        String telefone = edit_telefone.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String,Object> usuarios = new HashMap<>();
+        usuarios.put("nome",nome);
+        usuarios.put("telefone",telefone);
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+    private void IniciarComponentes() {
+        edit_nome = findViewById(R.id.edit_nome);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        edit_cpf = findViewById(R.id.edit_cpf);
+        edit_placa = findViewById(R.id.edit_placa);
+        edit_renavam = findViewById(R.id.edit_renavam);
+        edit_data = findViewById(R.id.edit_data);
+        edit_telefone = findViewById(R.id.edit_telefone);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+    }
+
+    private void CadastrarUsuario(final View v) {
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            SalvarDadosUsuario();
+                            Snackbar snackbar = Snackbar.make(v, mensagens[1], Snackbar.LENGTH_SHORT);
+                            snackbar.setBackgroundTint(Color.BLACK);
+                            snackbar.setTextColor(Color.WHITE);
+                            snackbar.show();
+                        }else{
+                            String erro;
+                            try {
+                                throw task.getException();
+
+                            }catch (FirebaseAuthWeakPasswordException e) {
+                                erro = "Digite uma senha com no mínimo 6 caracteres";
+                            }catch (FirebaseAuthUserCollisionException e) {
+                                erro = "Este e-mail já está em uso.";
+                            }catch (FirebaseAuthInvalidCredentialsException e) {
+                                erro = "E-mail inválido";
+                            }catch (Exception e){
+                                erro = "Erro ao cadastrar usuário";
+                            }
+
+                            Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
+                            snackbar.setBackgroundTint(Color.BLACK);
+                            snackbar.setTextColor(Color.WHITE);
+                            snackbar.show();
+                        }
+                    }
+                });
     }
 }
 
