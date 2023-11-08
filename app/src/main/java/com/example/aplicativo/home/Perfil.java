ddb9ba7e6842c9f1e46eadfe290574b1cb0607d3
brenomohr <breno.mohr1@gmail.com>
2023-11-07 21:55:00 -0300

    Alteração inclusão do método de recuperação de nome e e-mail

--- app/src/main/java/com/example/aplicativo/home/Perfil.java
+++ app/src/main/java/com/example/aplicativo/home/Perfil.java
@@ -1,19 +1,37 @@
 package com.example.aplicativo.home;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.aplicativo.Login;
 import com.example.aplicativo.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class Perfil extends AppCompatActivity {
+
+    private TextView emailUser, placaVeiculo, nomeUsuario;
+    private Button deslogar;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+        IniciarComponentes();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setSelectedItemId(R.id.bottom_perfil);
@@ -37,6 +55,46 @@ public class Perfil extends AppCompatActivity {
             }
             return true;
         });
-        //Fim
+
+        deslogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(Perfil.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if(documentSnapshot != null){
+                    nomeUsuario.setText(documentSnapshot.getString("nome"));
+                    emailUser.setText(email);
+                }
+            }
+        });
+    }
+
+    private void IniciarComponentes() {
+        nomeUsuario = findViewById(R.id.nomeUsuario);
+        emailUser = findViewById(R.id.emailUser);
+        deslogar = findViewById(R.id.deslogar);
+
     }
 }
+
